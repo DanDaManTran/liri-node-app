63403f91a22e10c6edf8825d1dec30e7555deb76
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2017-01-09 23:14:44 -0600

    removing keycodes from key.js -DT

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key:	 'jHGgkRp7azddxzU3skpHNAtAU',
-  consumer_secret: 'AmqDhG10VYRfoUDwuMF3JrEb8eWXqVN2NXmDj7LhOr1YrF2zql',
-  access_token_key: '804366545975644161-g7bkZw8U2e6nWJvTpHevRvcZE0jlKxn',
-  access_token_secret: 	'nEHqw2mONucHgNPfPtYGix5iuPFun9uIADEMOH7h2wbZW',
-}
